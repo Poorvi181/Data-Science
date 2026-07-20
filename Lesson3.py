dbd482995650be02f6c4aa5f342767fa58064c1b
Poorvi181 <mannarchana@yahoo.co.uk>
2026-07-20 19:36:38 +0100

    Add files via upload

---
+++ Lesson3.py
@@ -0,0 +1,27 @@
+# 1D ARRAYS \
+
+# ADD
+
+import numpy as np
+
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+
+c = a + b
+
+print(c)
+
+# Sub
+
+c = a - b
+print(c)
+
+# mul
+
+c = a * b
+print(c)
+
+# div
+ 
+c = a / b
+print(c)
---
+++ Lesson3_c.py
@@ -0,0 +1,22 @@
+#3D ARRAYS
+
+import numpy as np
+
+a = np.array([[1, 2], [3,4], [5,6], [7,8]])
+b = np. array ([[10,20], [30,40], [50,60], [70,80]])
+
+#Add
+
+print("Addition: \n\n", a + b)
+
+# Sub
+
+print("\nSubtraction: \n\n", a - b)
+
+# Mul
+
+print("\nMultiplication: \n\n", a * b)
+
+# Div
+
+print("\nDivision: \n\n", a / b)
